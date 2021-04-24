fd99a887406f86fc57a823a2cde405546eaeff6a
Pavel Kasila <pavel.kasila@gmail.com>
2021-04-24 15:54:28 +0300

    Update to 0.0.1-r12

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         .target(
             name: "SwiftP2P",
             dependencies: ["BlueP2PKit"]),
-        .binaryTarget(name: "BlueP2PKit", url: "https://raw.githubusercontent.com/BlueP2P/prebuilt/main/bluep2pkit/0.0.1-r11/BlueP2PKit.xcframework.zip", checksum: "10d890a644e598db988dd3dc1bb7678722b60973c6ca6c99987d52e804da6bd5"),
+        .binaryTarget(name: "BlueP2PKit", url: "https://raw.githubusercontent.com/BlueP2P/prebuilt/main/bluep2pkit/0.0.1-r12/BlueP2PKit.xcframework.zip", checksum: "7feb9f1539d9431a19e2819d2f2039526a5f484dfe87e7511a356d81439c4490"),
         .testTarget(
             name: "SwiftP2PTests",
             dependencies: ["SwiftP2P"]),
